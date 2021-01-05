82975e7679d06e19ccd1e6a39327a72b47c244af
Sierra Wallace <sskwallace@gmail.com>
2021-01-05 22:19:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-      ATTRIBU
+      ATTRIBUTES
   
   def self.table_name
     self.to_s.downcase.pluralize
